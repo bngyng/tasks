9dc488d68c179d6769c15e9a12439fd4df5552cd
bingyang@udel.edu <bingyang@udel.edu>
2026-02-16 23:29:41 -0500

    Completed shout function

--- src/functions.ts
+++ src/functions.ts
@@ -29,7 +29,8 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    let loudMessage: string = message.toUpperCase();
+    return loudMessage + "!";
 }
 
 /**
