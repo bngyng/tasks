8885c16c62a6d6923cc0ab439cd0cf15b40f743b
bingyang@udel.edu <bingyang@udel.edu>
2026-03-03 23:51:30 -0500

    Uncommented RevealAnswer line.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
-import { ChangeType } from "./components/ChangeType";
+// import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
-import { StartAttempt } from "./components/StartAttempt";
-import { TwoDice } from "./components/TwoDice";
-import { CycleHoliday } from "./components/CycleHoliday";
+// import { StartAttempt } from "./components/StartAttempt";
+// import { TwoDice } from "./components/TwoDice";
+// import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
 
 function App(): React.JSX.Element {
@@ -18,13 +18,13 @@ function App(): React.JSX.Element {
             <hr />
             <RevealAnswer></RevealAnswer>
             <hr />
-            <StartAttempt></StartAttempt>
-            <hr />
-            <TwoDice></TwoDice>
-            <hr />
-            <ChangeType></ChangeType>
-            <hr />
-            <CycleHoliday></CycleHoliday>
+            {/* <StartAttempt></StartAttempt> */}
+            {/* <hr /> */}
+            {/* <TwoDice></TwoDice> */}
+            {/* <hr /> */}
+            {/* <ChangeType></ChangeType> */}
+            {/* <hr /> */}
+            {/* <CycleHoliday></CycleHoliday> */}
         </div>
     );
 }
