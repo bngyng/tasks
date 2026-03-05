b22ef402b9137812619a49bab6ab2b6f21bb7369
bingyang@udel.edu <bingyang@udel.edu>
2026-03-04 22:06:03 -0500

    Uncommented CycleHoliday.

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import "./App.css";
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
 import { StartAttempt } from "./components/StartAttempt";
-// import { TwoDice } from "./components/TwoDice";
-// import { CycleHoliday } from "./components/CycleHoliday";
+import { TwoDice } from "./components/TwoDice";
+import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
 
 function App(): React.JSX.Element {
@@ -20,11 +20,11 @@ function App(): React.JSX.Element {
             <hr />
             <StartAttempt></StartAttempt>
             <hr />
-            {/* <TwoDice></TwoDice> */}
-            {/* <hr /> */}
+            <TwoDice></TwoDice>
+            <hr />
             <ChangeType></ChangeType>
             <hr />
-            {/* <CycleHoliday></CycleHoliday> */}
+            <CycleHoliday></CycleHoliday>
         </div>
     );
 }
