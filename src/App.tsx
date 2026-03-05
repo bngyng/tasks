eda77834a0c72200f3436426fe512eb91d351e78
bingyang@udel.edu <bingyang@udel.edu>
2026-03-04 20:33:04 -0500

    Uncommented StartAttempt.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./App.css";
-// import { ChangeType } from "./components/ChangeType";
+import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
-// import { StartAttempt } from "./components/StartAttempt";
+import { StartAttempt } from "./components/StartAttempt";
 // import { TwoDice } from "./components/TwoDice";
 // import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
@@ -18,12 +18,12 @@ function App(): React.JSX.Element {
             <hr />
             <RevealAnswer></RevealAnswer>
             <hr />
-            {/* <StartAttempt></StartAttempt> */}
-            {/* <hr /> */}
+            <StartAttempt></StartAttempt>
+            <hr />
             {/* <TwoDice></TwoDice> */}
             {/* <hr /> */}
-            {/* <ChangeType></ChangeType> */}
-            {/* <hr /> */}
+            <ChangeType></ChangeType>
+            <hr />
             {/* <CycleHoliday></CycleHoliday> */}
         </div>
     );
