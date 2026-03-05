30747b9ffaee1208cd410dd81607ecd277eb7f06
bingyang@udel.edu <bingyang@udel.edu>
2026-03-04 22:06:13 -0500

    Completed.

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,33 @@ export function d6(): number {
 }
 
 export function TwoDice(): React.JSX.Element {
-    return <div>Two Dice</div>;
+    const [leftValue, setLeftValue] = useState<number>(1);
+    const [rightValue, setRightValue] = useState<number>(2);
+
+    function rollLeftDie(): void {
+        setLeftValue(d6());
+    }
+    function rollRightDie(): void {
+        setRightValue(d6());
+    }
+
+    return (
+        <div>
+            <div>Two Dice</div>
+            <br></br>
+            <span data-testid="left-die">{leftValue}</span>
+            <span data-testid="right-die">{rightValue}</span>
+            <div>
+                <Button onClick={rollLeftDie}>Roll Left</Button>
+                <Button onClick={rollRightDie}>Roll Right</Button>
+            </div>
+            <div>
+                {leftValue === rightValue ?
+                    leftValue === 1 ?
+                        "You Lose!"
+                    :   "You Win!"
+                :   ""}
+            </div>
+        </div>
+    );
 }
