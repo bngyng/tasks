dbc83f056b9eac13a3843024d934d02415823e61
bingyang@udel.edu <bingyang@udel.edu>
2026-03-04 20:32:57 -0500

    Completed.

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,37 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): React.JSX.Element {
-    return <div>Start Attempt</div>;
+    const [attempts, setAttempts] = useState<number>(4);
+    const [isInProgress, setIsInProgress] = useState<boolean>(false);
+
+    function startQuiz(): void {
+        setIsInProgress(true);
+        setAttempts(attempts - 1);
+    }
+    function stopQuiz(): void {
+        setIsInProgress(false);
+    }
+    function addMulligan(): void {
+        setAttempts(attempts + 1);
+    }
+
+    return (
+        <div>
+            <div>Attempts remaining: {attempts}</div>
+            <Button
+                onClick={startQuiz}
+                disabled={isInProgress || attempts === 0}
+            >
+                Start Quiz
+            </Button>
+            <Button onClick={stopQuiz} disabled={!isInProgress}>
+                Stop Quiz
+            </Button>
+            <Button onClick={addMulligan} disabled={isInProgress}>
+                Mulligan
+            </Button>
+        </div>
+    );
 }
+
+
