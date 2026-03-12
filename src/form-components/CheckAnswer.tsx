5e514992e59cba8ea9794abb96a6a9763c1d6661
bingyang@udel.edu <bingyang@udel.edu>
2026-03-12 01:56:43 -0400

    Completed.

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,26 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
-export function CheckAnswer({
-    expectedAnswer,
-}: {
-    expectedAnswer: string;
-}): React.JSX.Element {
+
+export function CheckAnswer({expectedAnswer}: {expectedAnswer: string;}): React.JSX.Element {
+    const [givenAnswer, updateGivenAnswer] = useState<string>("");
+
+    function inputAnswer(event: React.ChangeEvent<HTMLInputElement>){
+        updateGivenAnswer(event.target.value);
+    }
     return (
         <div>
             <h3>Check Answer</h3>
+            <Form.Group controlId="formCheckAnswer">
+                <Form.Label>Enter Answer</Form.Label>
+                <Form.Control 
+                    value={givenAnswer}
+                    onChange={inputAnswer} />
+            </Form.Group>
+            <div>
+                {givenAnswer===expectedAnswer? "✔️":"❌"}
+            </div>
         </div>
+
     );
 }
