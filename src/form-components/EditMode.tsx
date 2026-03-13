2d0edd3f317866e3115b3d9531f3258807323c8d
bingyang@udel.edu <bingyang@udel.edu>
2026-03-12 21:49:45 -0400

    Completed.

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -2,16 +2,15 @@ import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 
 export function EditMode(): React.JSX.Element {
-    
     const [name, setName] = useState<string>("Your Name");
     const [isStudent, setIsStudent] = useState<boolean>(true);
-    const [editMode, enterEditMode]=useState<boolean>(false);
+    const [editMode, enterEditMode] = useState<boolean>(false);
 
-    function updateEditMode(event: React.ChangeEvent<HTMLInputElement>){
+    function updateEditMode(event: React.ChangeEvent<HTMLInputElement>) {
         enterEditMode(event.target.checked);
     }
 
-    function updateName(event: React.ChangeEvent<HTMLInputElement>){
+    function updateName(event: React.ChangeEvent<HTMLInputElement>) {
         setName(event.target.value);
     }
 
@@ -26,14 +25,21 @@ export function EditMode(): React.JSX.Element {
                 onChange={updateEditMode}
             />
             {editMode ?
-                <><Form.Group controlId="studentNane">
-                    <Form.Label>Enter Student Name:</Form.Label>
-                    <Form.Control value={name} onChange={updateName} />
-                </Form.Group><Form.Check
+                <>
+                    <Form.Group controlId="studentName">
+                        <Form.Label>Enter Student Name:</Form.Label>
+                        <Form.Control value={name} onChange={updateName} />
+                    </Form.Group>
+                    <Form.Check
                         type="checkbox"
+                        id="student-check"
                         label="Student?"
                         checked={isStudent}
-                        onChange={(event) => {setIsStudent(event.target.checked)}} /></>
+                        onChange={(event) => {
+                            setIsStudent(event.target.checked);
+                        }}
+                    />
+                </>
             :   <div>
                     {name} is {isStudent ? "a student" : "not a student"}
                 </div>
